4264e28e7300ff600b4cd0b4cf1fe967a8fe338b
Hank Wu <hank@m1air.myfiosgateway.com>
2025-02-17 20:53:05 -0500

    GenAiDemo Source Code

---
+++ server.js
@@ -0,0 +1,40 @@
+// const express = require("express");
+// const cors = require("cors")
+// const {GoogleGenerativeAI} = require("@google/generative-ai")
+
+
+
+import express from 'express'
+import cors from 'cors'
+import {GoogleGenerativeAI} from "@google/generative-ai"
+
+const genAI = new GoogleGenerativeAI("YOUR API KEY")
+const model = genAI.getGenerativeModel({model: "gemini-2.0-flash"})
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.post("/submit-form", async (req, res) => {
+    const {content} = req.body;
+    console.log("Received: ", req.body);
+
+    const prompt = content
+    const result = await model.generateContent(prompt)
+
+    // console.log(result)
+    console.log(result.response.text())
+
+    res.json({message: result.response.text()})
+});
+
+app.listen(5500, () => {
+    console.log("I am the server handling request backend on port 5500")
+})
+
+
+
+
+
+
+
